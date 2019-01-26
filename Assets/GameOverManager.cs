5d2a6ee238e4cd805f55a2e97dd88c667bff50ac
Celia Porcher <celiaporcher@gmail.com>
2019-01-26 18:21:04 +0100

    sounds theme integration

--- Assets/GameOverManager.cs
+++ Assets/GameOverManager.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class GameOverManager : MonoBehaviour
 {
+    public AudioSource audiosource;
+    public AudioClip gameoversound;
+
+    void Start()
+    {
+        audiosource.clip = gameoversound;
+        audiosource.loop = true;
+        audiosource.Play();
+    }
+
     public void NextScene()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Start", UnityEngine.SceneManagement.LoadSceneMode.Single);
