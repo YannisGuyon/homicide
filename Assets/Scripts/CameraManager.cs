db68c4ab1a5dbf00f4eb1b7a42b5b0493933ae16
Yannis Guyon <yannisguyon@hotmail.com>
2019-01-26 04:28:43 +0100

    Initial commit

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Camera cam;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public CameraManager camera_manager;
+    public SceneManager scene_manager;
+    public UiManager ui_manager;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
